f64e4507fed2e98530bb4644290f92217a5ddc65
ComfortTC <mothalez374@gmail.com>
2024-09-28 11:13:48 +0200

    server changes

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
+
 # PostgreSQL configuration (Heroku environment variable)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://localhost/dbname'
 db = SQLAlchemy(app)
