63a8fc546ac7888dca265f749bc6a35c82b88a95
ComfortTC <mothalez374@gmail.com>
2024-10-07 19:47:41 +0200

    test

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect(url_for('list_jobs'))  # Redirect to the job listing page
+    return redirect(url_for('resume'))  # Serve the resume page by default
+
+@app.route('/resume')
+def resume():
+    return render_template('index.html')  # Render the resume template
 
 @app.route('/favicon.ico')
 def favicon():
@@ -29,38 +33,6 @@ class Job(db.Model):
 # Create database tables
 with app.app_context():
     db.create_all()
-
-# Routes for CRUD operations
-@app.route('/admin/jobs', methods=['GET'])
-def list_jobs():
-    jobs = Job.query.all()
-    return render_template('index.html', jobs=jobs)
-
-@app.route('/admin/job/new', methods=['POST'])
-def create_job():
-    title = request.form['title']
-    company = request.form['company']
-    description = request.form['description']
-    new_job = Job(title=title, company=company, description=description)
-    db.session.add(new_job)
-    db.session.commit()
-    return redirect(url_for('list_jobs'))
-
-@app.route('/admin/job/delete/<int:id>', methods=['POST'])
-def delete_job(id):
-    job = Job.query.get_or_404(id)
-    db.session.delete(job)
-    db.session.commit()
-    return redirect(url_for('list_jobs'))
-
-@app.route('/admin/job/update/<int:id>', methods=['POST'])
-def update_job(id):
-    job = Job.query.get_or_404(id)
-    job.title = request.form['title']
-    job.company = request.form['company']
-    job.description = request.form['description']
-    db.session.commit()
-    return redirect(url_for('list_jobs'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
